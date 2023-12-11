dd9a8f76a92abf1e4dab4bb7dcc6864d13e25611
gartoks <gartoks@users.noreply.github.com>
2023-12-11 23:14:09 +0100

    bugfixes, debug helpers
    - fixed fairy bread collider in toasted game mode
    - added debug helper method to convert collider coordinates

--- BlobGame/DEBUGStuff.cs
+++ BlobGame/DEBUGStuff.cs
@@ -1,4 +1,6 @@
 ﻿using Raylib_CsLo;
+using System.Diagnostics;
+using System.Numerics;
 using System.Text.Json;
 
 namespace BlobGame;
@@ -27,5 +29,40 @@ internal static class DEBUGStuff {
 
         Dictionary<string, int>? dict = JsonSerializer.Deserialize<Dictionary<string, int>>(json);
         NPatchInfo npi2 = new NPatchInfo(new Rectangle(0, 0, 500, 400), dict["left"], dict["right"], dict["top"], dict["bottom"], (NPatchLayout)dict["layout"]);
+    }
+
+    public static void DEBUG_ConvertColliderCoords() {
+        string str = @"145,1,
+1294,1,
+
+1392,44,
+
+1440,149,
+1440,1291,
+
+1392,1387,
+
+1284,1440,
+155,1440,
+
+42,1390,
+0,1291,
+0,150,
+
+53,40";
+
+        // The size of the vertices in texture coords
+        const float OLD_SIZE = 1440f;
+        // The size of the vertices in game coords
+        const float NEW_SIZE = 360f;
+
+        Vector2[] vertices = str.Replace("\r", "").Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries).Select(l => {
+            string[] c = l.Replace(',', ';').Split(';');
+            return new Vector2(float.Parse(c[0]), float.Parse(c[1]));
+        }).ToArray();
+
+        Vector2[] newVertices = vertices.Select(v => v / OLD_SIZE * NEW_SIZE - new Vector2(NEW_SIZE / 2f, NEW_SIZE / 2f)).ToArray();
+
+        Debug.WriteLine(string.Join(",", newVertices.Select(v => $"{v.X},{v.Y}")));
     }
 }
